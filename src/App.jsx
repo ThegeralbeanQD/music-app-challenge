7c7c4f6bfbb01919289dd3a323f72b664163e4b1
“thegeralbeanQD” <giquigeraldine@outlook.com>
2023-05-15 17:35:25 +0100

    add styles for cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 import artists from './assets/artists.json'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 // console.log(artists.length);
 
 export default function App() {
   return (
-      <section>
-        {artists.map((artist) => (
-          <ArtistCard key={artist.id} artist={artist} />
-        ))}
-      </section>
+    <section>
+      {artists.map((artist) => (
+        <ArtistCard key={artist.id} artist={artist} />
+      ))}
+    </section>
   )
 }
 
@@ -24,13 +25,15 @@ const LikeToggle = () => {
   };
 
   const buttonStyle = {
-    backgroundColor: like ? 'red' : 'white',
+    color: like ? '#d93a3a' : 'black',
   };
 
   return (
-    <button onClick={handleLikeToggle} style={buttonStyle} className='likeButton'>
-      {like ? 'Unlike' : 'Like'}
+    <button onClick={handleLikeToggle} className='likeButton'>
+      <FontAwesomeIcon icon={faHeart} size='2xl' className='buttonIcon' style={buttonStyle} />
+      <span className='buttonText'>{like ? 'Unlike' : 'Like'}</span>
     </button>
+
   );
 };
 
@@ -38,14 +41,14 @@ function ArtistCard({ artist }) {
   return (
     <div className='artistCard'>
       <h2 className='artistName'>{artist.name}</h2>
-      <img src={artist.imgURL} className='artistPhoto'/>
+      <img src={artist.imgURL} className='artistPhoto' />
       <p className="artist-description">{artist.description}</p>
       <h3>Top Albums</h3>
       <div className="album-covers">
         {artist.topSoldAlbums.map((album) => (
           <div key={album.albumName} className='album-holder'>
-            <img src={album.albumCover} alt={album.albumName} className="album-cover"/>
-            <p>{album.albumName}</p>
+            <img src={album.albumCover} alt={album.albumName} className="album-cover" />
+            <h4>{album.albumName}</h4>
             <p>{album.releaseDate}</p>
           </div>
         ))}
